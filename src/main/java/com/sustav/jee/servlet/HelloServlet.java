08b05909e7439bc2a2331cf21e80632a61164f61
Anton Sustavov <Anton_Sustavov@epam.com>
2019-07-15 18:40:14 +0300

    add servlet

--- src/main/java/com/sustav/jee/servlet/HelloServlet.java
+++ src/main/java/com/sustav/jee/servlet/HelloServlet.java
@@ -1,6 +1,18 @@
 package com.sustav.jee.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello");
+    }
 }
